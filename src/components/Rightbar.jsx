e439d7563e7c4454be80d9b9774b45c4b387d064
adegoke bestman <adegokebestman@gmail.com>
2023-08-16 13:54:28 +0100

    social-media

---
+++ src/components/Rightbar.jsx
@@ -0,0 +1,156 @@
+import {
+    Avatar,
+    AvatarGroup,
+    Box,
+    Divider,
+    ImageList,
+    ImageListItem,
+    List,
+    ListItem,
+    ListItemAvatar,
+    ListItemText,
+    Typography,
+  } from "@mui/material";
+  import React from "react";
+
+  const Rightbar = () => {
+    return (
+      <Box flex={2} p={2} sx={{ display: { xs: "none", sm: "block" } }}>
+        <Box position="fixed" width={300}>
+          <Typography variant="h6" fontWeight={100}>
+            Online Friends
+          </Typography>
+          <AvatarGroup max={7}>
+            <Avatar
+              alt="Remy Sharp"
+              src="https://material-ui.com/static/images/avatar/1.jpg"
+            />
+            <Avatar
+              alt="Travis Howard"
+              src="https://material-ui.com/static/images/avatar/2.jpg"
+            />
+            <Avatar
+              alt="Cindy Baker"
+              src="https://material-ui.com/static/images/avatar/3.jpg"
+            />
+            <Avatar alt="Agnes Walker" src="" />
+            <Avatar
+              alt="Trevor Henderson"
+              src="https://material-ui.com/static/images/avatar/6.jpg"
+            />
+            <Avatar
+              alt="Trevor Henderson"
+              src="https://material-ui.com/static/images/avatar/7.jpg"
+            />
+            <Avatar
+              alt="Trevor Henderson"
+              src="https://material-ui.com/static/images/avatar/8.jpg"
+            />
+            <Avatar
+              alt="Trevor Henderson"
+              src="https://material-ui.com/static/images/avatar/7.jpg"
+            />
+            <Avatar
+              alt="Trevor Henderson"
+              src="https://material-ui.com/static/images/avatar/8.jpg"
+            />
+          </AvatarGroup>
+          <Typography variant="h6" fontWeight={100} mt={2} mb={2}>
+            Latest Photos
+          </Typography>
+          <ImageList cols={3} rowHeight={100} gap={5}>
+            <ImageListItem>
+              <img
+                src="https://images.unsplash.com/photo-1682695795557-17447f921f79?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"
+                alt="person"
+              />
+            </ImageListItem>
+            <ImageListItem>
+              <img
+                src="https://images.unsplash.com/photo-1691145697326-aa4fa5da7174?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=464&q=80"
+                alt=""
+              />
+            </ImageListItem>
+            <ImageListItem>
+              <img
+                src="https://material-ui.com/static/images/image-list/camera.jpg"
+                alt="building"
+              />
+            </ImageListItem>
+          </ImageList>
+          <Typography variant="h6" fontWeight={100} mt={2}>
+            Latest Conversations
+          </Typography>
+          <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
+        <ListItem alignItems="flex-start">
+          <ListItemAvatar>
+            <Avatar alt="Remy Sharp" src="https://material-ui.com/static/images/avatar/3.jpg" />
+          </ListItemAvatar>
+          <ListItemText
+            primary="Brunch this weekend?"
+            secondary={
+              <React.Fragment>
+                <Typography
+                  sx={{ display: 'inline' }}
+                  component="span"
+                  variant="body2"
+                  color="text.primary"
+                >
+                  Ali Connors
+                </Typography>
+                {" — I'll be in your neighborhood doing errands this…"}
+              </React.Fragment>
+            }
+          />
+        </ListItem>
+        <Divider variant="inset" component="li" />
+        <ListItem alignItems="flex-start">
+          <ListItemAvatar>
+            <Avatar alt="Travis Howard" src="https://images.unsplash.com/photo-1498565275770-43a2f5bf27cd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80" />
+          </ListItemAvatar>
+          <ListItemText
+            primary="Summer BBQ"
+            secondary={
+              <React.Fragment>
+                <Typography
+                  sx={{ display: 'inline' }}
+                  component="span"
+                  variant="body2"
+                  color="text.primary"
+                >
+                  to Scott, Alex, Jennifer
+                </Typography>
+                {" — Wish I could come, but I'm out of town this…"}
+              </React.Fragment>
+            }
+          />
+        </ListItem>
+        <Divider variant="inset" component="li" />
+        <ListItem alignItems="flex-start">
+          <ListItemAvatar>
+            <Avatar alt="Cindy Baker" src="https://images.unsplash.com/photo-1524660988542-c440de9c0fde?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80" />
+          </ListItemAvatar>
+          <ListItemText
+            primary="Femi Johnson"
+            secondary={
+              <React.Fragment>
+                <Typography
+                  sx={{ display: 'inline' }}
+                  component="span"
+                  variant="body2"
+                  color="text.primary"
+                >
+                  Sandra Adams
+                </Typography>
+                {' — Did you get the Job?'}
+              </React.Fragment>
+            }
+          />
+        </ListItem>
+      </List>
+        </Box>
+      </Box>
+    );
+  };
+
+  export default Rightbar;
